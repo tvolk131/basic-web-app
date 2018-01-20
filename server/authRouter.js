1e929bbe2a8f0a9ff0724c20a93ed2e1558d1960
Thomas Volk <tvolk131@gmail.com>
2018-01-20 01:42:27 -0600

    Removed unused local auth API routes

--- server/authRouter.js
+++ server/authRouter.js
@@ -9,16 +9,6 @@ router.get('/auth/google/callback', passport.authenticate('google', {
   failureRedirect: '/login'
 }));
 
-// Allows passport local login on this page
-router.post('/login', passport.authenticate('local-signin', {
-  successRedirect: '/'
-}));
-
-// Allows passport local signup on this page
-router.post('/signup', passport.authenticate('local-signup', {
-  successRedirect: '/'
-}));
-
 // Destroys current session when entering this page (for any passport strategy)
 router.get('/logout', (req, res) => {
   req.session.destroy((err) => {
