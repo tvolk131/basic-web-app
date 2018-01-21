6951d8282cfab6ba8e251506d37dd30ed7031f42
Thomas Volk <tvolk131@gmail.com>
2018-01-20 18:16:01 -0600

    Fixed mockDB

--- spec/database/mockDBHelpers.js
+++ spec/database/mockDBHelpers.js
@@ -1,11 +1,11 @@
 const mockDB = require('./mockDB.json');
-const {User} = require('../../database');
+const { User } = require('../../database');
 
 module.exports.createUsers = () => {
   let promises = [];
   mockDB.users.forEach((user) => {
     promises.push(
-      User.model.create(user)
+      User.create(user)
     );
   });
   return Promise.all(promises);
