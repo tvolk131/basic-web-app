ab425ee8cda2caa38f13f7ec25c55c504d594f51
Thomas Volk <tvolk131@gmail.com>
2018-02-17 09:57:42 -0600

    Transferring server from express to hapi

--- server/index.js
+++ server/index.js
@@ -1,117 +1,42 @@
 require('dotenv').config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const db = require('../database');
-const url = require('url');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const authRouter = require('./authRouter.js');
-const passportSocketIo = require('passport.socketio');
-const socketHandler = require('./socketHandler.js');
-const compression = require('compression');
-const session = require('express-session');
-const graphQLSchema = require('./graphql');
-const expressGraphQL = require('express-graphql');
-const secret = process.env.SESSION_SECRET;
+const fs = require('fs');
+const port = process.env.PORT;
+const Hapi = require('hapi');
+const server = new Hapi.Server({port});
 
+const html = fs.readFileSync(`${__dirname}/../client/dist/index.html`).toString();
+const vendor = fs.readFileSync(`${__dirname}/../client/dist/vendor.js`).toString();
+const bundle = fs.readFileSync(`${__dirname}/../client/dist/bundle.js`).toString();
 
-const shouldCompress = (req, res) => {
-  if (req.headers['x-no-compression']) {
-    // Don't compress responses with this request header
-    return false;
+server.route([
+  {
+    method: 'GET',
+    path: '/{any*}',
+    handler: (request, reply) => {
+      let user = {};
+      let friends = [];
+      let requestsSent = [];
+      let requestsReceived = [];
+      return `<script>window.__PRELOADED_STATE__ = {global: {user: ${JSON.stringify(user)}}, friend: {friends: ${JSON.stringify(friends)}, requestsSent: ${JSON.stringify(requestsSent)}, requestsReceived: ${JSON.stringify(requestsReceived)}}}</script>` + html;
+    }
+  },
+  {
+    method: 'GET',
+    path: '/vendor.js',
+    handler: (request, reply) => {
+      return vendor;
+    }
+  },
+  {
+    method: 'GET',
+    path: '/bundle.js',
+    handler: (request, reply) => {
+      return bundle;
+    }
   }
-  return compression.filter(req, res);
-};
+]);
 
-// Initialize passport strategies
-require('./auth')(db.User);
+const io = require('socket.io')(server.listener);
 
-let app = express();
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
-
-app.use(compression({filter: shouldCompress}));
-
-// ---- MIDDLEWARE ----
-// Body parser
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-app.use(cookieParser());
-// Passport and sessions
-app.use(session({
-  store: db.store,
-  secret,
-  resave: true,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Setup auth and api routing
-app.use('/graphql', expressGraphQL((request, response, graphQLParams) => (
-  {schema: graphQLSchema, graphiql: !(process.env.NODE_ENV === 'production')}
-)));
-app.use('/', authRouter); // Middleware redirector
-
-// Serve static files
-app.get('*/bundle.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/../client/dist/bundle.js'));
-});
-app.get('*/vendor.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/../client/dist/vendor.js'));
-});
-app.get('/*', (req, res) => {
-  if (req.user) {
-    db.Friend.getAll(req.user.id)
-      .then(({sent, received, friends}) => {
-        res.render('index', {
-          user: JSON.stringify(req.user),
-          friends: JSON.stringify(friends),
-          requestsSent: JSON.stringify(sent),
-          requestsReceived: JSON.stringify(received)
-        });
-      });
-  } else {
-    res.render('index', {
-      user: JSON.stringify(null),
-      friends: JSON.stringify([]),
-      requestsSent: JSON.stringify([]),
-      requestsReceived: JSON.stringify([])
-    });
-  }
-});
-
-let http = require('http').Server(app);
-let io = require('socket.io')(http);
-
-// Setup passport authentication for web sockets
-io.use(passportSocketIo.authorize({
-  key: 'connect.sid',
-  secret,
-  store: db.store,
-  passport,
-  cookieParser
-}));
-
-// Setup socket event handlers
-io.on('connection', (socket) => {
-  socketHandler.openSocket(socket);
-  socket.on('disconnect', () => {
-    socketHandler.closeSocket(socket);
-  });
-});
-
-// Launch/export server
-if (module.parent) {
-  module.exports = http;
-} else {
-  let port = process.env.PORT || 3000;
-  http.listen(port, () => {
-    console.log('Listening on port ' + port);
-  });
-}
+server.start().then(() => { console.log(`Server is running on port ${port}`); });
