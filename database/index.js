a20b7134a3e5d24bd4a2ed9854c29c90046937c7
Thomas Volk <tvolk131@gmail.com>
2018-02-18 02:56:25 -0600

    Removed unused session store

--- database/index.js
+++ database/index.js
@@ -1,9 +1,7 @@
-const store = require('./sessionStore');
 const User = require('./models/user');
 const Friend = require('./models/friend');
 
 module.exports = {
-  store,
   User,
   Friend
 };
--- database/sessionStore.js
+++
@@ -1,5 +0,0 @@
-// const uri = process.env.DB_HOST;
-// const session = require('express-session');
-// const MongoDBStore = require('connect-mongodb-session')(session);
-
-// module.exports = new MongoDBStore({uri, collection: 'sessions'});
