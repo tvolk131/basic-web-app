88f10b27c6f68d13e0c482e7900289ed4ce6e023
Thomas Volk <tvolk131@gmail.com>
2018-02-18 03:02:45 -0600

    Deleted unused files

--- server/auth/index.js
+++
@@ -1,7 +0,0 @@
-const googleAuth = require('./strategies/google.js');
-const userSerializeDeserialize = require('./user');
-
-module.exports = (userModel) => {
-  googleAuth(userModel);
-  userSerializeDeserialize(userModel);
-};
